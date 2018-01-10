e238cb75d4f5cc3b6f11360a328a12d97579cfa1
Raghu Betina <raghu@firstdraft.com>
2018-01-10 04:22:13 +0000

    Direct association between cusines and dishes

--- app/models/cusine.rb
+++ app/models/cusine.rb
@@ -1,6 +1,9 @@
 class Cusine < ApplicationRecord
   # Direct associations
 
+  has_many   :dishes,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,8 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  belongs_to :cusine
+
   has_many   :bookmarks,
              :dependent => :destroy
 
